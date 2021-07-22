05d570921703cce9dd9dd67562aa1bec39ca1df9
marilynmanson <nigin-ki18@stud.sfu-kras.ru>
2021-07-23 04:50:08 +0700

    Error processing was added

--- files/utils.py
+++ files/utils.py
@@ -1,6 +1,7 @@
 import csv
 from django.utils.timezone import datetime
 
+from files.models import File
 from topcustomers.models import Deal, Customer
 
 
@@ -8,48 +9,66 @@ def csv_to_db(file_data):
     """
     Функция сохраняет данные из csv файла в таблицы БД customers и deals
     :param file_data: Данные файла: имя и тд.
-    :return:
+    :return: Статус: OK или Error с описанием ошибки
     """
     file_path = file_data.path
     file_name = file_data.name
     with open(file_path, encoding='utf8', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         first_row = True
-        for row in reader:
-            if not first_row:
-                customer_name = row[0]  # Extract customer's name
-                item = row[1]
-                total = int(row[2])
-                quantity = int(row[3])
-                date = datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S.%f")
+        try:
+            for row in reader:
+                # Если читаемый ряд в файле состоит не из 5 элементов, то исключение
+                if not len(row) == 5:
+                    raise Exception('Incorrect data row. Details: wrong separation - there must be 5 fields')
 
-                new_deal = Deal(
-                    customer=customer_name,
-                    item=item,
-                    total=total,
-                    quantity=quantity,
-                    date=date,
-                    file_name=file_name
-                )
+                if not first_row:
+                    customer_name = row[0]  # Extract customer's name
+                    item = row[1]
 
-                try:
-                    customer = Customer.objects.get(name=customer_name, file_name=file_name)
-                except Customer.DoesNotExist:
-                    customer = None
+                    try:
+                        total = int(row[2])
+                        quantity = int(row[3])
+                    except Exception as e:
+                        raise Exception('Incorrect data row. Details: ' + str(e))
 
-                if customer:
-                    customer.spent_money += total
-                    gems = set(customer.gems.split(','))
-                    gems.add(item)
-                    customer.gems = ','.join(gems)
-                else:
-                    customer = Customer(
-                        name=customer_name,
-                        spent_money=total,
-                        gems=item,
+                    try:
+                        date = datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S.%f")
+                    except Exception as e:
+                        raise Exception('Incorrect data row. Details: ' + str(e))
+
+                    new_deal = Deal(
+                        customer=customer_name,
+                        item=item,
+                        total=total,
+                        quantity=quantity,
+                        date=date,
                         file_name=file_name
                     )
-                new_deal.save()
-                customer.save()
-            else:
-                first_row = False
+
+                    try:
+                        customer = Customer.objects.get(name=customer_name, file_name=file_name)
+                    except Customer.DoesNotExist:
+                        customer = None
+
+                    if customer:
+                        customer.spent_money += total
+                        gems = set(customer.gems.split(','))
+                        gems.add(item)
+                        customer.gems = ','.join(gems)
+                    else:
+                        customer = Customer(
+                            name=customer_name,
+                            spent_money=total,
+                            gems=item,
+                            file_name=file_name
+                        )
+                    new_deal.save()
+                    customer.save()
+                else:
+                    first_row = False
+        except Exception as e:
+            File.objects.get(file=file_data).delete()
+            return {'Status': {'Error': str(e)}}
+
+        return {'Status': 'OK'}
--- files/views.py
+++ files/views.py
@@ -1,4 +1,6 @@
+from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, mixins
 from files.models import File
 from files.serializers import FileSerializer
@@ -10,9 +12,20 @@ class FilesViewSet(mixins.ListModelMixin,
                    GenericViewSet):
     queryset = File.objects.all()
     serializer_class = FileSerializer
-    parser_classes = (MultiPartParser, FormParser, )
+    parser_classes = (MultiPartParser, FormParser,)
+    csv_to_db_result = {}  # Хранит результат сохранения данных файла в БД
 
     def perform_create(self, serializer):
         file_data = self.request.data.get('file')
         file_obj = serializer.save(file=file_data)
-        csv_to_db(file_obj.file)
+        self.csv_to_db_result = csv_to_db(file_obj.file)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        if self.csv_to_db_result['Status'] == 'OK':
+            return Response(self.csv_to_db_result, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(self.csv_to_db_result, status=status.HTTP_400_BAD_REQUEST, headers=headers)
